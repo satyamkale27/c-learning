ac1908eab22212f5ad30334b60991a31bcb837c8
satyam kale <satyamkale271@gmail.com>
2023-02-15 21:17:16 +0530

    half way mofification

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ int main()
     int characterage = 35;
     printf("there once was a man named %s\n",charactername);
     printf("his age was %d\n",characterage);
+
+characterage = 60; // modified the value in half way
+
     printf("he really liked liked the name %s\n",charactername);
     printf("but did not be like to be %d\n",characterage);
     return 0;
