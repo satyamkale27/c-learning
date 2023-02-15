7b0a7fce2c9d1f125548cbed7223b5deab71f895
satyam kale <satyamkale271@gmail.com>
2023-02-15 21:28:11 +0530

    basic data types

---
+++ datatype.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+int age = 40;  // it can store only integer value 
+double gpa = 3.6; // it is used to store decimals it can also store 3.0
+char grade = 'A'; // it is used to store character single 'A' not 'ABFG'
+
+
+
+
+
+
+return 0;
+
+}
