3b513f101f506b66b5936decdf69194b32e17b50
satyam kale <satyamkale271@gmail.com>
2023-02-15 21:45:14 +0530

    add printf function and format specifier

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+printf("my favorite number is %d",500); // %d is format specifiyer why allows to print numbers anf print f is a function
+
+
+
+
+
+
+    return 0;
+}
