e7378c2d883d7126b483f6af7ac6338c58d1cf92
satyam kale <satyamkale271@gmail.com>
2023-02-15 22:24:28 +0530

    more maths function added

--- numbers.c
+++ numbers.c
@@ -5,7 +5,9 @@ int main()
 {
 
 printf("%f", pow(2, 3) ); // it is power function it gives 2^3
+printf("%f", sqrt(36) );  // it shows square root 
 
+printf("%f", floor(36.669) ); // it round offs the number
 
     return 0;
 }
