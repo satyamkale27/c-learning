58e700c009e708cc675e7182e141f71e40b4561c
satyam kale <satyamkale271@gmail.com>
2023-02-18 22:36:11 +0530

    done with user input

---
+++ userinput.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // int age;
+    double gpa;
+    // printf("Enter your age:");
+    // scanf("%d", &age);
+    // printf("you are %d years old", age);
+
+
+    printf("Enter your gpa:");
+    scanf("lf", &gpa);
+    printf("your gpa is %lf", gpa);
+
+
+
+
+
+    return 0;
+}
