fae8dd2f33656ba687c04251b6935be4adc775c6
satyam kale <satyamkale271@gmail.com>
2023-02-22 22:30:35 +0530

    created calculator using c

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+double num1;
+double num2;
+printf("Enter the number 1:");
+scanf("%lf",&num1);
+printf("Enter the number 2:");
+scanf("%lf",&num2);
+printf("the sum of number is :%f", num1 + num2);
+
+
+return 0;
+
+
+
+}
