95245b5441f031a05c115c1a78f63c217507cc07
satyam kale <satyamkale271@gmail.com>
2023-02-25 17:54:19 +0530

    learned array

---
+++ Array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+// int luckynum[] = {4, 8, 15, 16, 23, 42};
+// // luckynum[0] = 200;   this changes the value of 0th index array
+// printf("%d",luckynum[0]);
+
+int luckynum[10];
+luckynum[0] = 16;
+printf("%d",luckynum[0]);
+
+
+
+    return 0;
+}
