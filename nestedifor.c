253f27ebddac983155f1f21289e6fbf5a0e54dc1
satyam kale <satyamkale271@gmail.com>
2023-03-01 15:57:09 +0530

    || uses in if statments

---
+++ nestedifor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int num1 = 3;
+int num2 = 9;
+
+if(num1 >= num2 || num2 >= num1) {
+printf("if one is true it gets executed");
+
+} else {
+    printf("somthing is wrong");
+}
+
+    return 0;
+}
