544350abe3eda1430facdfc1be58212efceae049
satyam kale <satyamkale271@gmail.com>
2023-03-03 14:57:05 +0530

    learned while loop

---
+++ loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+// while loop//
+
+int index;
+printf("Enter the index:");
+scanf("%d", &index);
+while ( index <= 5)
+{
+    printf("%d\n", index);
+    index++;
+}
+
+
+    return 0;
+}
