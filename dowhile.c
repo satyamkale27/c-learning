b610563c2df750bce77e8c560e45aec6d34909a1
satyam kale <satyamkale271@gmail.com>
2023-03-03 15:07:32 +0530

    learned dowhile loop

---
+++ dowhile.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+// do while loop runs and then check the condition//
+int index = 6;
+
+do
+{
+    printf("%d\n", index);
+    index++;
+} while (index <=5);
+
+
+
+
+
+
+
+    return 0;
+}
