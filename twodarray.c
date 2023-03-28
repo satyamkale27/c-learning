b3fa62ebee3bf3357962acc62ca287918db5fd78
satyam kale <satyamkale271@gmail.com>
2023-03-28 22:17:27 +0530

    twodarray.c modified(changed location)

---
+++ twodarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+int nums[3] [2] = {
+
+// there are three array in this array and each array has two element //
+
+      {1, 2}; // this is 0th element //
+      {3, 4}; // this is 1th element //
+      {5, 6}; // this is 2th element //
+};
+
+printf("%d", nums[0] [0]);
+
+
+    return 0;
+}
