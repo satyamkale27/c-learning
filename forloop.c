426079a9a0b92278c4ec388a5b7e6b686603829b
satyam kale <satyamkale271@gmail.com>
2023-03-28 21:54:01 +0530

    learned forloop

---
+++ forloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+int luckynumber[] = {4, 8, 15, 16, 23, 42};
+int i;
+
+for (i = 0; i < 6; i++)
+{
+    printf("%d\n",luckynumber[i]);
+}
+
+    return 0;
+}
