2229c75e194d51099ff8d2e949a3fb0706234668
satyam kale <satyamkale271@gmail.com>
2023-03-28 22:19:29 +0530

    twodarray.c modified(changed location)+ typo corrected

--- twodarray.c
+++ twodarray.c
@@ -6,9 +6,9 @@ int nums[3] [2] = {
 
 // there are three array in this array and each array has two element //
 
-      {1, 2}; // this is 0th element //
-      {3, 4}; // this is 1th element //
-      {5, 6}; // this is 2th element //
+      {1, 2}, // this is 0th element //
+      {3, 4}, // this is 1th element //
+      {5, 6} // this is 2th element //
 };
 
 printf("%d", nums[0] [0]);
