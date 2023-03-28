84b3b37414af07c3dd4b6db6aea1f7f86a818d1a
satyam kale <satyamkale271@gmail.com>
2023-03-28 23:28:28 +0530

    learned memory adress

---
+++ memoryadress.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+int age = 19;
+double gpa = 9.9;
+char grade = 'A';
+
+printf("%p", &age);    // %p represents physical memory adress
+    return 0;
+}
