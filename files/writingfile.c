2ffae0bb5dd48d2d359815c5b599829e54f45e49
satyam kale <satyamkale271@gmail.com>
2023-03-29 23:59:22 +0530

    employees.txt updated

--- files/writingfile.c
+++ files/writingfile.c
@@ -2,8 +2,13 @@
 #include<stdlib.h>
 int main() {
 
-FILE * fpointer = fopen("employees.txt", "w");
-fclose(fpointer);
+// FILE * fpointer = fopen("employees.txt", "w"); // w means write //
+ // fprintf(fpointer,"jim, salesman\nkevin, accountant\ndavid, software developer");
+     // fprintf used to write files//
+FILE * fpointer = fopen("employees.txt", "a");
+fprintf(fpointer,"\nkelly, customer service"); // a is used to write newely //
+
+fclose(fpointer); 
 
 
     return 0;
