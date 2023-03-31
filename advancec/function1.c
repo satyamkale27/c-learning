ac18cad120727794ed26e567ae632110e607aa49
satyam kale <satyamkale271@gmail.com>
2023-03-31 23:03:42 +0530

    function1.c

---
+++ advancec/function1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void myfunction() {
+
+printf("hello my name is kevin"); // this function do not take any parameter //
+
+}
+
+
+int main() {
+
+
+myfunction();  // here i have called the function //
+
+    return 0;
+}
