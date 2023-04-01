8438f3ce5e0360d589cbd806fa95223a412b9c74
satyam kale <satyamkale271@gmail.com>
2023-04-01 21:16:05 +0530

    fuction3.c updated

--- advancec/function3.c
+++ advancec/function3.c
@@ -8,6 +8,8 @@ printf("Hello %s\n.you are %d", grade, age);
 int main() {
 
 myfunction("kevin", 19);
+// Note that when you are working with multiple parameters, the function call must have the same number of arguments as there are parameters,
+// and the arguments must be passed in the same order.
 
     return 0;
 }
