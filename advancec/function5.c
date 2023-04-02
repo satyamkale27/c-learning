ab496473735de1b85132c14837571d066163e220
satyam kale <satyamkale271@gmail.com>
2023-04-02 19:25:19 +0530

    function5.c

---
+++ advancec/function5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ void myfunction(); // function decelaration //
+
+int main() {
+
+myfunction(); // calling the function //
+
+    return 0;
+}
+
+void myfunction() {
+printf("i got excuted");
+}
