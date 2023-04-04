c49851fe7ba49924dd52b9281b9e03e865c4461d
satyam kale <satyamkale271@gmail.com>
2023-04-04 22:26:32 +0530

    mathfunction.c done

---
+++ advancec/mathfunction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+int a;
+
+printf("Enter the number:");
+scanf("%d", &a);
+printf("%f", sqrt(a));
+
+// explore https://www.w3schools.com/c/c_math.php to learn more //
+
+    return 0;
+}
